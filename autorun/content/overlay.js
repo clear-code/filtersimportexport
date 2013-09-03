ebaf05d229cbd9dc5000944558e4b7eb8d2042d5
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2013-09-03 12:36:03 +0900

    Add module to operate preferences

--- autorun/content/overlay.js
+++ autorun/content/overlay.js
@@ -2,3 +2,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+var filtersimportexportAutorun = {
+  get prefs() {
+    delete this.prefs;
+    var ns = {};
+    Components.utils.import('resource://folderstatistics-modules/prefs.js', ns);
+    return this.prefs = ns.prefs;
+  },
+
+  run: function filtersimportexportAutorun_run() {
+  }
+};
