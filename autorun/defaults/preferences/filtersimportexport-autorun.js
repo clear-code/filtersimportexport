f3d9cd2811838b28f74a293a8f4a886e30a18af4
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2013-09-03 12:36:21 +0900

    Add sample preferences for the autorun module

--- autorun/defaults/preferences/filtersimportexport-autorun.js
+++ autorun/defaults/preferences/filtersimportexport-autorun.js
@@ -2,3 +2,19 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// // This is the root key. If this is false, these rules will be ignored.
+// pref("extensions.filtersimportexport-autorun@clear-code.com.rules.KEY", true);
+// // export source
+// pref("extensions.filtersimportexport-autorun@clear-code.com.rules.KEY.from",
+//      "accountName:user@example.com"); // by the name
+// pref("extensions.filtersimportexport-autorun@clear-code.com.rules.KEY.from",
+//      "incomingServer:user@example.com"); // by the incoming server
+// pref("extensions.filtersimportexport-autorun@clear-code.com.rules.KEY.from",
+//      "none@Local Folder"); // local folder account
+// // import target
+// pref("extensions.filtersimportexport-autorun@clear-code.com.rules.KEY.to",
+//      "incomingServer:user@example.com"); // the format is same to "from"
+// // results: this addon automatically saves the result as this pref.
+// pref("extensions.filtersimportexport-autorun@clear-code.com.rules.KEY.done",
+//      "serverkey1,serverkey2");
+
